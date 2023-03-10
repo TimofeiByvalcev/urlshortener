e6e2d864ffede5ba01de74a8b3aed8f3caf8e23e
Timofei Byvalcev <timofei.byvalcev@saritasa.com>
2023-03-10 17:03:12 +0500

    Add main.py file, define create_url endpoint, define not valid url exception

--- urlshortener_app/main.py
+++ urlshortener_app/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
+
+from . import schemas
+
+# Define app instantiating the FastAPI class
 
 app = FastAPI()
 
 
+# Use a path operation decorator to associate your root path with read_root() by registering it in FastAPI
 @app.get("/")
 def read_root():
     return "Welcome to the API of URL shortener"
+
+
+# Define an exception if provided URL isn't valid
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+
+# Define create_url endpoint which expects a URL string as POST request body
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided url isn't valid")
+    return f"TODO: Create db entry for : {url.target_url}"
