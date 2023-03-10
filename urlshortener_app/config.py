459c0b48ed2da02d69d98c082fa73625d37bc95e
Timofei Byvalcev <timofei.byvalcev@saritasa.com>
2023-03-10 15:30:51 +0500

    Add Config class in config.py

--- urlshortener_app/config.py
+++ urlshortener_app/config.py
@@ -1,16 +1,21 @@
-#urlshortener/config.py
+# urlshortener/config.py
 
 from pydantic import BaseSettings
+from functools import lru_cache
 
 
-class Settings(BaseSettings):
+class LocalSettings(BaseSettings):
     env_name: str = "Local"
     base_url: str = "http://localhost:8000"
     db_url: str = "sqlite:///./shoretener.db"
 
 
-def get_settings() -> Settings:
-    settings = Settings()
+@lru_cache()
+def get_settings() -> LocalSettings:
+    settings = LocalSettings()
     print(f"Loading setting for: {settings.env_name}")
     return settings
 
+
+class Config:
+    env_file = ".env"
